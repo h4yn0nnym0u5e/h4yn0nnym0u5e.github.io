4e1a152da1026e6dd23a7d689ecc9ce8e10ea5e6
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2025-04-25 12:00:26 +0100

    Implement per URL/index design storage
    See https://github.com/h4yn0nnym0u5e/Audio/tree/feature/GUI-per-path-storage

--- gui/red/storage.js
+++ gui/red/storage.js
@@ -3,17 +3,37 @@
  */
 
 RED.storage = (function() {
+	function target()
+	{
+		var result = "audio_library_guitool";
+		var useLocation = true; // set false to return to old behaviour
+		if (useLocation)
+		{
+			var url = window.location.href;
+			// Use the changeable part of the URL, but
+			// allow for "index-with-more-stuff.html"
+			// files with different sets of objects
+			var root = url.search(/gui[/]index.*[.]htm/);
+			if (root >= 0)
+			{
+				root = url.indexOf(".htm")
+				url = url.slice(0, root);
+				result = url + "/" + result
+			}
+		}
+		return result
+	}
 	function update() {
 		// TOOD: use setTimeout to limit the rate of changes?
 		if (localStorage) {
 			var nns = RED.nodes.createCompleteNodeSet();
-			localStorage.setItem("audio_library_guitool", JSON.stringify(nns));
+			localStorage.setItem(target(), JSON.stringify(nns));
 			//console.log("localStorage write");
 		}
 	}
 	function load() {
 		if (localStorage) {
-			var data = localStorage.getItem("audio_library_guitool");
+			var data = localStorage.getItem(target());
 			//console.log("localStorage read: " + data);
 			if (data) RED.nodes.import(data, false);
 		}
@@ -21,7 +41,7 @@ RED.storage = (function() {
 	function clear() {
 		// TOOD: use setTimeout to limit the rate of changes?
 		if (localStorage) {
-			localStorage.removeItem("audio_library_guitool");
+			localStorage.removeItem(target());
 			//console.log("localStorage write");
 		}
 	}
